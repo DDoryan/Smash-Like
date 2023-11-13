b87b57dc4031aee70b67ad6546ccf34e6129baa7
DDoryan <118185694+DDoryan@users.noreply.github.com>
2023-11-13 14:14:14 +0100

    Movements update
    Ajustement of the values of movements for a smoother gameplay

--- Smash/Assets/Scripts/PlayerController.cs
+++ Smash/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,5 @@
+using System;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,9 +7,9 @@ public class PlayerController : MonoBehaviour
 {
     private Vector3 playerVelocity;
 
-    private float playerSpeed = 30.0f;
+    private float playerSpeed = 100.0f;
     private float jumpHeight = 10.0f;
-    private float gravityValue = -30f;
+    private float gravityValue = -30;
 
     private Vector2 movementInput = Vector2.zero;
     private bool jumped = false;
@@ -15,35 +17,62 @@ public class PlayerController : MonoBehaviour
     private Transform _transform;
     public Transform _raycastOrigin;
     private Vector2 gravity = new Vector2(0f, -30f);
-    public float raycastDist = 0.005f;
+    private Vector2 counterForce = new Vector2(0f, 0f);
+    public float groundedDist = 0.006f;
     private bool grounded = false;
-    private float jumpCooldown = 0.5f;
+    private bool nextToTheGround = false;
+    private float nextToTheGroundDist = 1f;
+    private float jumpCooldown = 0.1f;
     private float jumpTimer;
+    private Vector2 move = Vector2.zero;
+    private float controllerDeadZone = 0.3f;
+    private float maxSpeedX = 10f;
+    private RaycastHit2D hit;
 
     private void Awake()
     {
         _transform = transform;
         _rigidBody2D = GetComponent<Rigidbody2D>();
         jumpTimer = Time.time;
-        jumpCooldown = 1f;
     }
 
     void FixedUpdate()
     {
+        hit = Physics2D.Raycast(_raycastOrigin.position, Vector3.down, 2);
         grounded = IsGrounded();
+        nextToTheGround = IsNextToTheGround();
+        if (_rigidBody2D.velocity.y < 0f)
+        {
+            gravity.y = -65f;
+        }
+        else
+        {
+            gravity.y = -30f;
+        }
+
         _rigidBody2D.AddForce(gravity);
-        if (grounded && playerVelocity.y < 0)
+        if (movementInput.magnitude < controllerDeadZone && movementInput.magnitude > -controllerDeadZone)
         {
-            playerVelocity.y = 0f;
+            counterForce.x = -_rigidBody2D.velocity.x * 10f;
+            _rigidBody2D.AddForce(counterForce);
+            Debug.Log("not move");
+        }
+        else
+        {
+            move.x = movementInput.x;
+            move.y = 0;
+            _rigidBody2D.AddForce(move * playerSpeed * Time.deltaTime, ForceMode2D.Impulse);
+            Debug.Log("move");
         }
 
-        Vector3 move = new Vector3(movementInput.x, 0, 0);
-        Debug.Log(movementInput);
-
-        if (move != Vector3.zero)
+        if (grounded && _rigidBody2D.velocity.y < 0)
         {
-            _rigidBody2D.AddForce(move * playerSpeed * Time.deltaTime,ForceMode2D.Impulse);
+            move.Set(_rigidBody2D.velocity.x, 0);
+            _rigidBody2D.velocity = move;
         }
+        
+        move.Set( Math.Clamp(_rigidBody2D.velocity.x, -maxSpeedX, maxSpeedX), _rigidBody2D.velocity.y);
+        _rigidBody2D.velocity = move;
 
         if (grounded)
         {
@@ -51,7 +80,7 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-            _rigidBody2D.drag = 2f;
+            _rigidBody2D.drag = 3f;
         }
 
         if (jumped && grounded && Time.time > jumpTimer) 
@@ -59,9 +88,6 @@ public class PlayerController : MonoBehaviour
             playerVelocity.y = jumpHeight * -3.0f * gravityValue;
             _rigidBody2D.AddForce(playerVelocity);
             jumpTimer = Time.time + jumpCooldown;
-            jumped = false;
-        }else
-        {
             jumped = false;
         }
     }
@@ -73,7 +99,7 @@ public class PlayerController : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (context.performed && nextToTheGround)
         {
             jumped = true;
         }
@@ -81,10 +107,16 @@ public class PlayerController : MonoBehaviour
 
     public bool IsGrounded() 
     {
-        
-        RaycastHit2D hit = Physics2D.Raycast(_raycastOrigin.position, Vector3.down, raycastDist);
-        Debug.DrawLine(_raycastOrigin.position, new Vector3(_raycastOrigin.position.x, _raycastOrigin.position.y - raycastDist, _raycastOrigin.position.z), Color.red);
-        if (hit.collider != null && hit.collider.gameObject.tag == "ground")
+        if (hit.collider != null && hit.collider.gameObject.tag == "ground" && Vector2.Distance(hit.point, _raycastOrigin.position) < groundedDist)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool IsNextToTheGround()
+    {
+        if (hit.collider != null && hit.collider.gameObject.tag == "ground" && Vector2.Distance( hit.point, _raycastOrigin.position) < nextToTheGroundDist)
         {
             return true;
         }
